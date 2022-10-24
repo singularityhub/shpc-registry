82dff4769226c122659688aa27aa45d80aa8f024
vsoch <vsoch@users.noreply.github.com>
2022-10-24 05:41:50 -0600

    first round of work to add new biocontainers with aliases
    this will preview the addition and test the workflow.
    We will likely want to run this once a month (before
    the registry is updated) and it cannot be merged until
    a new update strategy is in place. I have an idea based
    on taking hashes of names, and then breaking those down
    equally into chunks to run on different days of the month.
    More TBA!
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/update_biocontainers.py
+++ .github/scripts/update_biocontainers.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
+# This script uses count data from https://github.com/singularityhub/shpc-registry-cache
+# to:
+
+# - Subsetting to those in a container, including counts
+# - Rank ordering from least to greatest (lower frequency is a more unique command)
+# - Including any counts with a frequency < 10
+# - Above that, including the next N
+# - Use these to generate a new container.yaml for the file (if it does not exist yet)
+
+# It requires a local clone of shpc-registry-cache with a counts file available,
+# along with a json file with commands found there.
+# Usage:
+# python .github/scripts/update_biocontainers.py --cache ../shpc-registry-cache --registry $PWD
+
 import argparse
 import os
+import collections
 from shpc.main import get_client
-from container_guts.main import ManifestGenerator
 import shpc.utils
-import shutil
-import requests
-import re
-import glob
-from bs4 import BeautifulSoup
-
-import pipelib.steps as step
-import pipelib.pipeline as pipeline
-
-# Pre-generated sets of steps we can use
-import pipelib.pipelines as pipelines
-
+import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
 root = os.path.dirname(os.path.dirname(here))
@@ -27,44 +30,20 @@ def get_parser():
         description="SHPC BioContainer Adder",
         formatter_class=argparse.RawTextHelpFormatter,
     )
+    parser.add_argument("--cache", help="Path to cache with counts.json.")
     parser.add_argument("--registry", help="Path to registry root.", default=root)
     parser.add_argument("--maintainer", help="Author", default="@vsoch")
     parser.add_argument(
-        "--cache",
-        help="cache to save container aliases",
-        default=os.path.join(root, ".cache"),
+        "--min-count-inclusion", help="Author", default=10, type=int, dest="min_count"
+    )
+    parser.add_argument(
+        "--additional-count-inclusion",
+        help="Author",
+        default=25,
+        type=int,
+        dest="add_count",
     )
     return parser
-
-
-# A pipeline to process docker tags
-steps = (
-    # Scrub commits from version string
-    step.filters.CleanCommit(),
-    # Parse versions, return sorted ascending, and taking version major.minor.patch into account
-    step.container.ContainerTagSort(),
-)
-p = pipeline.Pipeline(steps)
-
-
-def get_tags(links, registry):
-    uris = {}
-
-    # Use the latest for each unique
-    for link in links:
-        if ":" not in link.text:
-            continue
-        image, tag = link.text.split(":", 1)
-
-        # If we already have the registry folder, skip
-        container_dir = os.path.join(registry, "quay.io", "biocontainers", image)
-        if image in uris or os.path.exists(container_dir):
-            continue
-
-        print(f"Retrieving tags for {image}")
-        tags = requests.get(f"https://crane.ggcr.dev/ls/quay.io/biocontainers/{image}")
-        uris[image] = [x for x in tags.text.split("\n") if x]
-    return uris
 
 
 def main():
@@ -81,119 +60,67 @@ def main():
     cli = get_client()
     cli.settings.registry = [args.registry]
     cli.reload_registry()
+    counts = os.path.join(args.cache, "counts.json")
 
-    response = requests.get("https://depot.galaxyproject.org/singularity/")
-    soup = BeautifulSoup(response.text, "html.parser")
-    links = soup.find_all("a")
+    for path in args.registry, args.cache:
+        if not os.path.exists(path):
+            sys.exit(f"{path} does not exist")
 
-    # Allow developer to provide tags in root
-    tags_json = os.path.join(root, "tags.json")
-    if os.path.exists(tags_json):
-        uris = shpc.utils.read_json(tags_json)
-    else:
-        uris = get_tags(links, args.registry)
+    # Read in counts
+    counts = shpc.utils.read_json(counts)
 
-    # Ensure our alias cache exists
-    if not os.path.exists(args.cache):
-        shpc.utils.mkdir_p(args.cache)
+    # For each entry in the cache (which might not be in our registry) check for it!
+    for entry in shpc.utils.recursive_find(
+        os.path.join(args.cache, "quay.io"), ".json"
+    ):
+        # We really only need the container uri,
+        basename = os.path.basename(entry)
+        image, tag = basename.replace(".json", "").split(":", 1)
+        image = "quay.io/biocontainers/%s" % image
 
-    # Keep track of counts
-    counts = {}
-
-    # For each uri, get latest version
-    for uri, tags in uris.items():
-
-        container_dir = os.path.join(args.registry, "quay.io", "biocontainers", uri)
-        if "UNAUTHORIZED" in tags[0] or os.path.exists(container_dir):
+        container_dir = os.path.join(args.registry, image)
+        if os.path.exists(container_dir):
             continue
 
-        # The updated and transformed items
-        ordered = p.run(list(tags), unwrap=False)
-        if not ordered:
-            continue
+        print(f"Image {image} found in cache and not in registry!")
+        aliases = shpc.utils.read_json(entry)
 
-        tag = ordered[0]._original
-        container = f"quay.io/biocontainers/{uri}:{tag}"
-        try:
-            aliases = cache_aliases(container, args.cache, uri, tag)
-        except:
-            for path in glob.glob("/tmp/guts*"):
-                shutil.rmtree(path)
-            continue
+        # Look up counts and always take the number under a threshold (10)
+        keepers = {}
+        for x, path in aliases.items():
+
+            # Always take the very unique ones!
+            if counts[x] <= args.min_count:
+                keepers[x] = path
+
+        # of the remaining we have, sorted by count, keep top N (lower numbers == more unique)
+        alias_counts = {x: counts[x] for x in aliases if x not in keepers}
+
+        # Lowest to highest
+        sorted_counts = list(collections.OrderedDict(alias_counts))
+
+        while args.add_count > 0:
+            keeper = sorted_counts.pop(0)
+            keepers[keeper] = aliases[keeper]
+            args.add_count -= 1
 
         for alias, _ in aliases.items():
             if alias not in counts:
                 counts[alias] = 0
             counts[alias] += 1
 
-    import IPython
-
-    IPython.embed()
-    sys.exit()
-
-    # Finally, use cache to write aliases to container.yaml
-    for uri, tags in uris.items():
-
-        container_dir = os.path.join(args.registry, "quay.io", "biocontainers", uri)
-        if "UNAUTHORIZED" in tags[0] or os.path.exists(container_dir):
-            continue
-
-        # The updated and transformed items
-        ordered = p.run(list(tags), unwrap=False)
-        if not ordered:
-            continue
-        tag = ordered[0]._original
-        container = f"quay.io/biocontainers/{uri}:{tag}"
+        # Now add the container, and use the tag
+        container = f"{image}:{tag}"
+        entry_name = image.split(os.sep)[-1]
 
         # First derive and save the aliases - we can filter based
-        add_container(cli, container, args.maintainer, uri, cache=args.cache)
+        try:
+            add_container(cli, container, args.maintainer, entry_name, aliases=keepers)
+        except:
+            print(f"Issue adding container {container}")
 
 
-def include_path(path):
-    """
-    Filter out binaries that are in system bins.
-    """
-    return "sbin" not in path and "/usr/bin" not in path and not path.startswith("/bin")
-
-
-def cache_aliases(container, cache, uri, tag):
-    """
-    Keep a cache of aliases to use later
-    """
-    letter = uri[0].lower()
-    filename = os.path.join(
-        cache, "quay.io", "biocontainers", letter, "%s:%s.json" % (uri, tag)
-    )
-    prefix = os.path.join(cache, "quay.io", "biocontainers", letter, "%s*" % uri)
-
-    # Any glob of the container
-    if os.path.exists(filename) or glob.glob(prefix):
-        return shpc.utils.read_json(filename)
-
-    # Generate guts
-    gen = ManifestGenerator()
-    manifests = gen.diff(container)
-
-    # Assemble aliases
-    aliases = {}
-    for path in list(manifests.values())[0]["diff"]["unique_paths"]:
-        name = os.path.basename(path)
-        if not include_path(path):
-            continue
-
-        if name in aliases:
-            print(f"Warning, duplicate alias {name}")
-        print(path)
-        aliases[name] = path
-
-    parent = os.path.dirname(filename)
-    shpc.utils.mkdir_p(parent)
-    print(f"Writing {filename} with aliases")
-    shpc.utils.write_json(aliases, filename)
-    return aliases
-
-
-def add_container(cli, container, maintainer, entry_name):
+def add_container(cli, container, maintainer, entry_name, aliases):
     """
     Add a container from a unique resource identifier.
     """
@@ -213,30 +140,8 @@ def add_container(cli, container, maintainer, entry_name):
         "description"
     ] = f"shpc-registry automated BioContainers addition for {entry_name}"
     result["url"] = f"https://biocontainers.pro/tools/{entry_name}"
-
-    try:
-
-        # Generate guts
-        gen = ManifestGenerator()
-        manifests = gen.diff(container)
-
-        # Assemble aliases
-        aliases = {}
-        for path in list(manifests.values())[0]["diff"]["unique_paths"]:
-            name = os.path.basename(path)
-            if not include_path(path, name):
-                continue
-
-            if name in aliases:
-                print(f"Warning, duplicate alias {name}")
-            print(path)
-            aliases[name] = path
-
+    if aliases:
         result["aliases"] = aliases
-        print(f"Writing with aliases to {container_yaml}")
-    except:
-        pass
-
     shpc.utils.write_yaml(result, container_yaml)
 
 
