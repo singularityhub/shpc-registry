f3b62b4ac823ec1638cb4932e0baa67f787ef7fb
vsoch <vsoch@users.noreply.github.com>
2022-10-27 17:24:36 -0600

    do not regenerate
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/update_biocontainers.py
+++ .github/scripts/update_biocontainers.py
@@ -145,8 +145,8 @@ def main():
         image = "quay.io/biocontainers/%s" % image
 
         container_dir = os.path.join(args.registry, image)
-        #if os.path.exists(container_dir):
-        #    continue
+        if os.path.exists(container_dir):
+            continue
 
         print(f"Image {image} found in cache and not in registry!")
         aliases = shpc.utils.read_json(entry)
